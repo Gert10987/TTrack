4775fe520d3a02be11f43e469219d9d8b2f44137
gert <pawel.kalasz@gmail.com>
2017-05-01 17:24:05 +0200

    Added positions of markers on map

--- TTrackWeb/src/main/webapp/static/js/myMap.js
+++ TTrackWeb/src/main/webapp/static/js/myMap.js
@@ -1,9 +1,17 @@
-function myMap() {
+function myMap(var lat, var lng) {
+
+var myLatLng = {lat: 52.2, lng: 21.01};
 
 var mapCanvas = document.getElementById("map");
 var mapOptions = {
     center: new google.maps.LatLng(52.2, 21.01), zoom: 10
     };
 var map = new google.maps.Map(mapCanvas, mapOptions);
+
+var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
 
 }
